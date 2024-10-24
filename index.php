01785b048fb7f39eee9ef8ab53042eafe7b5e7cc
Mattijn Witteveen <mattijnwitteveen@gmail.com>
2024-10-24 14:09:40 +0200

    create index with layout

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Homework PHP</title>
+        <link rel="stylesheet" href="styling.css">
+    </head>
+    <body>
+        <div>
+            <img src="index.jpg" alt="Coming Soon"> 
+        </div>
+    </body>
+</html>
