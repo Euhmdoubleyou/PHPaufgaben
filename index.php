42dd5a9518bee62fa48391e609aff98a0efbc1ec
Mattijn Witteveen <mattijnwitteveen@gmail.com>
2024-10-24 14:27:13 +0200

    initial setup idea staged. Created pages

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
         <title>Homework PHP</title>
         <link rel="stylesheet" href="styling.css">
     </head>
+
+<nav>
+    <ul>
+        <li><a href="index.php">Montag 21.10</a></li>
+        <li><a href="about.php">Dienstag 22.10</a></li>
+        <li><a href="contact.php">Mittwoch 23.10</a></li>
+        <li><a href="gallery.php">Donnerstag 24.10</a></li>
+    </ul>
+</nav>
     <body>
         <div>
             <img src="index.jpg" alt="Coming Soon"> 
